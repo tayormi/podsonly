1892cc7c5091b7cc3ac91194d8dfa8a6162d1d1e
Temitope Ajiboye <tayormi@gmail.com>
2023-07-15 11:13:20 -0300

    Fixed top widget on home

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -8,53 +8,64 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red.shade50,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 40,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'What do you want to listen to today?',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ]),
+              ),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: const [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    HomeCategory(title: 'Top Trending Podcasts', genre: ''),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    HomeCategory(title: 'Tech Podcasts', genre: 'Technology'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    HomeCategory(title: 'Politics', genre: 'Politics'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    HomeCategory(title: 'Business', genre: 'Business'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    HomeCategory(title: 'Business', genre: 'Business'),
+                  ],
                 ),
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'What do you want to listen to today?',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                HomeCategory(title: 'Top Trending Podcasts', genre: ''),
-                SizedBox(
-                  height: 20,
-                ),
-                HomeCategory(title: 'Tech Podcasts', genre: 'Technology'),
-                SizedBox(
-                  height: 20,
-                ),
-                HomeCategory(title: 'Politics', genre: 'Politics'),
-                SizedBox(
-                  height: 20,
-                ),
-                HomeCategory(title: 'Business', genre: 'Business'),
-                SizedBox(
-                  height: 20,
-                ),
-                HomeCategory(title: 'Business', genre: 'Business'),
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
